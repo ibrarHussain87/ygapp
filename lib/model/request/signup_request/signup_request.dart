398b11c9fd2d20c2e883ea4d306ec23465d3ae7c
Ezsol <asads3403@gmail.com>
2022-04-28 17:04:38 +0500

    update signup

--- lib/model/request/signup_request/signup_request.dart
+++ lib/model/request/signup_request/signup_request.dart
@@ -9,9 +9,12 @@ class SignUpRequestModel {
   String? operator;
   String? telephoneNumber;
 
-  SignUpRequestModel({this.name,this.email, this.password,this.countryId,this.cityStateId,this.operator,this.telephoneNumber});
+  SignUpRequestModel(
+
+      {this.name,this.email, this.password,this.countryId,this.cityStateId,this.operator,this.telephoneNumber});
 
   Map<String, dynamic> toJson() {
+
     Map<String, dynamic> map = {
       'name': name!.trim(),
       'email': email!.trim(),
